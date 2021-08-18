98b7347eb8532f5d2053569f79e90567f2c50304
AiYuki <khinhvu2707@gmail.com>
2021-08-18 19:55:52 +0700

    commit fix

--- 02-typescript/practice/typescript-shape/src/rectangle.ts
+++ 02-typescript/practice/typescript-shape/src/rectangle.ts
@@ -1,12 +1,12 @@
 import {Shape} from "./shape";
 
-export class Rectangle extends Shape implements IArea{
+export class Rectangle extends Shape implements IArea {
     constructor(x: number, y: number, private _width: number, private _height: number) {
         super(x, y);
     }
 
     area(): number {
-        return this._width*this._height;
+        return this._width * this._height;
     }
 
     get width() {
